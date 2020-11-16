4737060fa22fc295550349b0588b6299d6c7aa9f
layla  hammoud <laylahammoud63@gmail.com>
2020-11-16 12:12:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   const messages = [];
   const writeCards = (arr, eventName) => {
   for (let i=0; i<arr.length; i++ ) {
-  let message = 'Thank you, ' +  arr[i] + ', for the wonderful ' + eventName + 'gift!';
+  let message = 'Thank you, ' +  arr[i] + ', for the wonderful ' + eventName + ' gift!';
   messages.push(message);
   //messages.push( `Thank you, ${arr[i]}, for the wonderful ${keyword} gift! `);
   }
