5b347f3a9196ced0d61d0d0c820f06e3d3c213db
layla  hammoud <laylahammoud63@gmail.com>
2020-11-16 12:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 
   const messages = [];
-  const writeCards = (arr, eventName) => {
-  for (let i=0; i<arr.length; i++ ) {
-  let message = 'Thank you, ' +  arr[i] + ', for the wonderful ' + eventName + ' gift!';
+  const writeCards = (array, eventName) => {
+  for (let i=0; i<array.length; i++ ) {
+  let message = 'Thank you, ' +  array[i] + ', for the wonderful ' + eventName + ' gift!';
   messages.push(message);
-  //messages.push( `Thank you, ${arr[i]}, for the wonderful ${keyword} gift! `);
   }
   return messages;
 }
