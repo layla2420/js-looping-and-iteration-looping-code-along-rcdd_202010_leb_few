a6d12176ac33f656ae8766f0c9e8ac9cc4c4d6a4
layla  hammoud <laylahammoud63@gmail.com>
2020-11-16 11:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// Code your solutions in this file
-let countdown=10 ; while(countdown>=0){console.log(countdown--) ; }
+const writeCards = (arr, event) => {
+  for (let i=0; i<arr.length; i++ ) {
+  const message = 'Thank you, ' +  arr[i] + ', for the wonderful ' + event + 'gift!';
+//  `Thank you, ${arr[i]} for the wonderful ${event} gift! `;
+  const myArray = new Array ();
+  // const myArray = [];
+  let newMessage= myArray.push (message);
+  }
+  return myArray;
+}
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+// "Thank you, Ada, for the wonderful birthday gift!"
